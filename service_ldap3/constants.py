6ed474374b72c1418e42c621c95c721f8cb752e9
forcemain <forcemain@163.com>
2021-08-30 16:54:46 +0800

    :underage:

---
+++ service_ldap3/constants.py
@@ -0,0 +1,8 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+# Ldap3配置
+LDAP3_CONFIG_KEY = 'LDAP3'
---
+++ service_ldap3/core/dependencies/ldap.py
@@ -0,0 +1,67 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+import typing as t
+
+from ldap3 import Server
+from ldap3 import ServerPool
+from ldap3 import Connection
+from service_core.core.context import WorkerContext
+from service_ldap3.constants import LDAP3_CONFIG_KEY
+from service_core.core.service.dependency import Dependency
+
+
+class Ldap(Dependency):
+    """ Ldap依赖类 """
+
+    def __init__(
+            self,
+            alias: t.Text,
+            debug: bool = False,
+            srvlist_options: t.Optional[t.List[t.Dict[t.Text, t.Any]]] = None,
+            srvpool_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
+            connect_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
+            **kwargs: t.Text
+    ) -> None:
+        """ 初始化实例
+
+        @param alias: 配置别名
+        @param debug: 开启调试
+        @param srvlist_options: 实例配置
+        @param srvpool_options: 池子配置
+        @param connect_options: 连接配置
+        """
+        self.alias = alias
+        self.debug = debug
+        self.server_pool = None
+        self.srvlist_options = srvlist_options or []
+        self.srvpool_options = srvpool_options or {}
+        self.connect_options = connect_options or {}
+        super(Ldap, self).__init__(**kwargs)
+
+    def setup(self) -> None:
+        """ 声明周期 - 载入阶段 """
+        srvlist_options = self.container.config.get(f'{LDAP3_CONFIG_KEY}.{self.alias}.srvlist_options', default=[])
+        # 防止YAML中声明值为None
+        self.srvlist_options += (srvlist_options or [])
+        srvpool_options = self.container.config.get(f'{LDAP3_CONFIG_KEY}.{self.alias}.srvpool_options', default={})
+        # 防止YAML中声明值为None
+        self.srvpool_options = (srvpool_options or {}) | self.srvpool_options
+        self.srvpool_options.setdefault('servers', None)
+        self.server_pool = ServerPool(**self.srvpool_options)
+        for server_options in self.srvlist_options: self.server_pool.add(Server(**server_options))
+        connect_options = self.container.config.get(f'{LDAP3_CONFIG_KEY}.{self.alias}.connect_options', default={})
+        # 防止YAML中声明值为None
+        self.connect_options = (connect_options or {}) | self.connect_options
+        self.connect_options.setdefault('server', self.server_pool)
+
+    def get_instance(self, context: WorkerContext) -> t.Any:
+        """ 获取注入对象
+
+        @param context: 上下文对象
+        @return: t.Any
+        """
+        return Connection(**self.connect_options)
