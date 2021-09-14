3ba10d28a1f09178b1d7ea22f099b6dda8aae818
forcemain <forcemain@163.com>
2021-09-14 10:32:54 +0800

    :underage:

--- service_ldap3/core/connect.py
+++ service_ldap3/core/connect.py
@@ -4,20 +4,33 @@
 
 from __future__ import annotations
 
+import logging
 import typing as t
 
+from ldap3.utils.log import NETWORK
 from ldap3 import Connection as BaseConnection
+from ldap3.utils.log import set_library_log_detail_level
+from ldap3.utils.log import set_library_log_activation_level
 
 
 class Connection(BaseConnection):
     """ Ldap3通用连接 """
 
-    def __init__(self, *args: t.Any, base_dn: t.Optional[t.Text] = None, **kwargs: t.Text) -> None:
+    def __init__(
+            self,
+            *args: t.Any,
+            debug: t.Optional[bool] = None,
+            base_dn: t.Optional[t.Text] = None,
+            **kwargs: t.Text
+    ) -> None:
         """ 初始化实例
 
+        @param debug : 开启调试
         @param base_dn:根域名称
         @param args  : 位置参数
         @param kwargs: 命名选项
         """
         self.base_dn = base_dn or ''
+        debug and set_library_log_detail_level(NETWORK)
+        debug and set_library_log_activation_level(logging.DEBUG)
         super(Connection, self).__init__(*args, **kwargs)
--- service_ldap3/core/dependencies/ldap.py
+++ service_ldap3/core/dependencies/ldap.py
@@ -4,20 +4,17 @@
 
 from __future__ import annotations
 
-import logging
 import typing as t
 
 from ldap3 import NTLM
 from ldap3 import Server
 from ldap3 import ServerPool
 from logging import getLogger
-from ldap3.utils.log import NETWORK
 from service_ldap3.core.connect import Connection
 from service_core.core.context import WorkerContext
 from service_ldap3.constants import LDAP3_CONFIG_KEY
-from ldap3.utils.log import set_library_log_detail_level
+
 from service_core.core.service.dependency import Dependency
-from ldap3.utils.log import set_library_log_activation_level
 
 logger = getLogger(__name__)
 
@@ -31,7 +28,6 @@ class Ldap(Dependency):
     def __init__(
             self,
             alias: t.Text,
-            debug: bool = False,
             srvlist_options: t.Optional[t.List[t.Dict[t.Text, t.Any]]] = None,
             srvpool_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
             connect_options: t.Optional[t.Dict[t.Text, t.Any]] = None,
@@ -40,19 +36,15 @@ class Ldap(Dependency):
         """ 初始化实例
 
         @param alias: 配置别名
-        @param debug: 开启调试
         @param srvlist_options: 实例配置
         @param srvpool_options: 池子配置
         @param connect_options: 连接配置
         """
         self.alias = alias
         self.client = None
-        self.server_pool = None
         self.srvlist_options = srvlist_options or []
         self.srvpool_options = srvpool_options or {}
         self.connect_options = connect_options or {}
-        debug and set_library_log_detail_level(NETWORK)
-        debug and set_library_log_activation_level(logging.DEBUG)
         super(Ldap, self).__init__(**kwargs)
 
     def setup(self) -> None:
@@ -64,12 +56,12 @@ class Ldap(Dependency):
         # 防止YAML中声明值为None
         self.srvpool_options = (srvpool_options or {}) | self.srvpool_options
         self.srvpool_options.setdefault('servers', None)
-        self.server_pool = ServerPool(**self.srvpool_options)
-        for server_options in self.srvlist_options: self.server_pool.add(Server(**server_options))
+        server_pool = ServerPool(**self.srvpool_options)
+        for server_options in self.srvlist_options: server_pool.add(Server(**server_options))
         connect_options = self.container.config.get(f'{LDAP3_CONFIG_KEY}.{self.alias}.connect_options', default={})
         # 防止YAML中声明值为None
         self.connect_options = (connect_options or {}) | self.connect_options
-        self.connect_options.setdefault('server', self.server_pool)
+        self.connect_options.setdefault('server', server_pool)
         self.connect_options.setdefault('auto_bind', True)
         self.connect_options.setdefault('authentication', NTLM)
         self.connect_options.setdefault('pool_size', len(self.srvlist_options))
