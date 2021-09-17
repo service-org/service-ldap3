c76b6594e2d739a323c929727ae0c1a6c31f1f09
forcemain <forcemain@163.com>
2021-09-17 12:08:29 +0800

    :underage:

--- service_ldap3/core/dependencies/ldap.py
+++ service_ldap3/core/dependencies/ldap.py
@@ -65,7 +65,8 @@ class Ldap(Dependency):
         connect_options = self.container.config.get(f'{LDAP3_CONFIG_KEY}.{self.alias}.connect_options', default={})
         # 防止YAML中声明值为None
         self.connect_options = (connect_options or {}) | self.connect_options
-        self.connect_options.setdefault('auto_bind', True)
+        self.connect_options.setdefault('lazy', True)
+        self.connect_options.setdefault('auto_bind', False)
         self.connect_options.setdefault('server', server_pool)
         self.connect_options.setdefault('authentication', NTLM)
         # 开启心跳防止服务端断开
